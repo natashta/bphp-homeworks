27fcc0216810fc58a87396631f3cef8a2e6322ed
natashta <natashta@local>
2019-11-15 18:40:19 +0300

    'task01.2'

---
+++ 1.1-intro-and-branching/1.1.1/index.php
@@ -0,0 +1,28 @@
+<?php
+$variable = 3.14;
+
+if (is_bool ($variable)) {
+    $type = 'boolean';
+} elseif (is_float ($variable)) {
+    $type = 'float';
+} elseif (is_int ($variable)) {
+    $type = 'integer';
+} elseif (is_string ($variable)) {
+    $type = 'string';
+} elseif (is_null ($variable)) {
+    $type = 'null';
+} else {
+    $type = 'other';
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>bPHP - 1.1.1</title>
+</head>
+<body>
+    <p><?php echo $variable . ' is ' . $type ?></p>
+</body>
+</html>
