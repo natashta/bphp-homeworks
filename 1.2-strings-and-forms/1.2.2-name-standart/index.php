20c56076a649d3b18a09a2ec305b4d5d74ba21ab
natashta <natashta@local>
2019-11-19 20:28:12 +0300

    task1.2.2

---
+++ 1.2-strings-and-forms/1.2.2-name-standart/index.php
@@ -0,0 +1,17 @@
+<?php
+function mb_ucfirst($str) {
+    $fc = mb_strtoupper(mb_substr($str, 0, 1));
+    return $fc.mb_substr($str, 1);
+}
+
+$fname = mb_ucfirst($_POST['firstName']);
+$lname = mb_ucfirst($_POST['lastName']);
+$mname = mb_ucfirst($_POST['middleName']);
+
+$fullName = ($lname).' '.($fname).' '.($mname);
+$surnameAndInitials = $lname.'.'.mb_substr($fname, 0, 1).'.'.mb_substr($mname, 0, 1);
+$fio = mb_substr($fname, 0, 1).mb_substr($fname, 0, 1).mb_substr($mname, 0, 1);
+
+echo "Полное имя: $fullName<br/>";
+echo "Фамилия и инициалы: $surnameAndInitials<br/>";
+echo "Аббревиатура: $fio<br/>";
